356a3b4b6a07875f88fb53377cc7795f80362947
Field4 <lauren.eb.field@hotmail.com>
2023-09-03 22:11:29 +0100

    error in the data retrieval

--- main.py
+++ main.py
@@ -17,3 +17,8 @@ data = json.loads(data)
 token = data.get("token")
 
 print(token)
+
+
+dataReq = requests.post("https://opendata.nationalrail.co.uk/api/staticfeeds/4.0/ticket-restrictions",data={'X-Auth-Token':token})
+
+print(dataReq.status_code)
