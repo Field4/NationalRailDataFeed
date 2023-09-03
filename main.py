d21415118d3378736c0ae6b0d30c8880c0498571
Field4 <lauren.eb.field@hotmail.com>
2023-09-03 22:01:36 +0100

    authentication and token retrieval

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import os
 from dotenv import main
+import requests
+import json
 
 main.load_dotenv('./.env')
 
 username = os.getenv("uname")
+password = os.getenv("password")
 
-print(username)
+tokenReq = requests.post('https://opendata.nationalrail.co.uk/authenticate', data={'Content-Type':'application/x-www-form-urlencoded','username':username, 'password':password}, )
+
+print(tokenReq.status_code)
+
+data = tokenReq.text
+data = json.loads(data)
+token = data.get("token")
+
+print(token)
