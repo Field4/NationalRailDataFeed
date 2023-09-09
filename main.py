f18ee802fb570c5914db4b9ae1b24ffa39818626
Field4 <lauren.eb.field@hotmail.com>
2023-09-09 14:25:12 +0100

    converting the xml into a pd dataframe

--- main.py
+++ main.py
@@ -15,26 +15,30 @@ password = os.getenv("password")
 
 tokenReq = requests.post('https://opendata.nationalrail.co.uk/authenticate', data={'Content-Type':'application/x-www-form-urlencoded','username':username, 'password':password}, )
 
-#print(tokenReq.status_code)
+# print(tokenReq.status_code)
 
 dataTok = json.loads(tokenReq.text)
 token = dataTok.get("token")
 
-#print(token)
+# print(token)
 
 
 dataReq = requests.get("https://opendata.nationalrail.co.uk/api/staticfeeds/4.0/ticket-restrictions",headers={'X-Auth-Token':token})
 
-#print(dataReq.status_code)
+# print(dataReq.status_code)
 data = dataReq.text
 
 root = ET.fromstring(data)
 
-print(root.tag)
-print(root.attrib)
-print('children:')
+# next block found on 'https://saturncloud.io/blog/converting-xml-to-python-dataframe-a-comprehensive-guide/'
 
-for child in root:
-    print(child.tag, child.attrib)
+data = []
+for elem in root:
+    row = {}
+    for subelem in elem:
+        row[subelem.tag] = subelem.text
+    data.append(row)
+df = pd.DataFrame(data)
 
-print(root[0][3].text,root[0][4].text,root[0][5].text)
+# End block
+
